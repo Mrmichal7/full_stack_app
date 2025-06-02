c66b115060aae1cea1aaa27a88e0ebdfb0315362
Michal Jurczak <michal.jurczak@student.pk.edu.pl>
2025-06-02 15:41:00 +0200

    poprawka

--- frontend/src/routes/AppRouter.tsx
+++ frontend/src/routes/AppRouter.tsx
@@ -49,14 +49,6 @@ const App: React.FC = () => {
               </PrivateRoute>
             }
           />
-          <Route
-            path="/groups/:groupId/members"
-            element={
-              <PrivateRoute>
-                <GroupMembersPage />
-              </PrivateRoute>
-            }
-          />
           <Route path="/groups/:groupId/debts" element={<GroupDebtsPage />} />
           <Route path="/test" element={<TestApiComponent />} />
           <Route
